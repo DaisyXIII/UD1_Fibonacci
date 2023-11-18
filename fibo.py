b0ace7d6207145b07a5250b90475775e6e4afc37
DaisyXIII <Daisy_love_costa@hotmail.com>
2023-11-18 16:59:01 +0100

    Primera versión
    Primera versión completa del script

---
+++ fibo.py
@@ -0,0 +1,15 @@
+#Crea un script que genere la secuencia de Fibonacci
+#y retorna el número que está en una posición dada
+def fibonacci(posicion):
+    if posicion <= 0:
+        return []
+    elif posicion == 1:
+        return [0]
+    elif posicion == 2:
+        return [0, 1]
+    else:
+        secuencia = [0, 1]
+        while len(secuencia) < posicion:
+            next_num = secuencia[-1] + secuencia[-2]
+            secuencia.append(next_num)
+        return secuencia
